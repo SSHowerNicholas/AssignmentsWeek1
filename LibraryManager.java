3a8369c19ae836934554f43de2005552376e99e0
Nicholas <Nicholas@100.76.100.69>
2020-01-29 17:10:46 -0500

    Library Manager Librarian path mostly complete

--- LibraryManager.java
+++ LibraryManager.java
@@ -7,7 +7,9 @@ public class LibraryManager {
 	private Library[] branches;
 	
 	public LibraryManager() {
-		branches = new Library[1];
+		branches = new Library[2];
+		branches[0] = new Library();
+		branches[1] = new Library();
 		branches[0].setName("University Library");
 		branches[0].setLocation("Boston");
 		branches[1].setName("State Library");
@@ -24,7 +26,7 @@ public class LibraryManager {
 			switch (resp.toLowerCase()) {
 			case "1":
 			case "librarian":
-				libBranch();
+				libBranchOne();
 				break;
 			case "2":
 			case "administrator":
@@ -45,14 +47,14 @@ public class LibraryManager {
 		menuscan.close();
 	}
 
-	private void libBranch() {
+	private void libBranchOne() {
 		boolean liquit = true;
 		while (liquit) {
 			System.out.println("1) Enter Branch you manage \n2) Quit to previous");
 			String liresp = menuscan.next();
 			if ("1".equals(liresp)) {
 				System.out.println("you have selected branch management");
-				//TODO
+				libBranchTwo();
 			}
 			else if("2".equals(liresp)) {
 				liquit = false;
@@ -61,6 +63,38 @@ public class LibraryManager {
 				System.out.println("Unexpected input, please try again");
 			}
 		}
+	}
+	private void libBranchTwo() {
+		 boolean liquittwo = true;
+		 while(liquittwo) {
+			 System.out.println("Choose your branch:");
+			 for(int i = 0; i < branches.length; i++) {
+				 System.out.println((i+1) + ") " + branches[i].getName() + ", " + branches[i].getLocation());
+			 }
+			 System.out.println((branches.length + 1) + ") Quit to previous");
+			 String lirespon = menuscan.next();
+			 Integer inte;
+			 try{
+				inte = Integer.valueOf(lirespon); 
+			 }
+			 catch(NumberFormatException e) {
+				 System.out.println(" Unexpected input, please try again");
+				 continue;
+			 }
+			 if(inte > branches.length + 1 || inte <= 0) {
+				 System.out.println(" Unexpected input, please try again");
+			 }
+			 else if(inte == branches.length + 1){
+				 liquittwo = false;
+			 }
+			 else {
+			 libBranchThree(branches[inte-1]);
+			 }
+		 }
+	}
+	
+	private void libBranchThree(Library lib) {
+		System.out.println("You are now managing a branch");
 	}
 
 	private void adminBranch() {
